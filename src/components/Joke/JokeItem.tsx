d5f27eaeb888b8b55ad33a4fedf6f5439dff095d
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-18 23:35:39 +0600

    Changed jokeSet in JokeItem

--- src/components/Joke/JokeItem.tsx
+++ src/components/Joke/JokeItem.tsx
@@ -7,22 +7,15 @@ interface Props {
 }
 
 const JokeItem: React.FC<Props> = ({ joke, onJokeButton }) => {
-  let jokeSet = <p>Click the Joke for bad joke</p>;
-
-  if (joke.type === "single") {
-    jokeSet = (
+  const jokeSet =
+    joke.type === "single" ? (
       <p className="bg-primary rounded-3 p-3 text-white">{joke.joke}</p>
-    );
-  }
-
-  if (joke.type === "twopart") {
-    jokeSet = (
+    ) : (
       <div>
         <p className="bg-info rounded-3 p-3 text-white">- {joke.setup}</p>
         <p className="bg-warning rounded-3 p-3 text-white">- {joke.delivery}</p>
       </div>
     );
-  }
 
   return (
     <React.Fragment>

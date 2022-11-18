e33abcebc370b5064374273f5dd5fd29255f794b
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-18 16:50:22 +0600

    Renamed Movie comp to MovieItem

---
+++ src/components/MovieList/MovieItem.tsx
@@ -0,0 +1,39 @@
+import React, { MouseEventHandler } from "react";
+import { Movie } from "../../types";
+
+interface Props {
+  movie: Movie;
+  onInputChange: (id: string, newName: string) => void;
+  onButtonClick: React.MouseEventHandler;
+}
+
+class MovieItem extends React.Component<Props> {
+  shouldComponentUpdate(nextProps: Readonly<Props>): boolean {
+    return nextProps.movie.name !== this.props.movie.name;
+  }
+
+  render() {
+    return (
+      <label
+        className={"d-flex"}
+        style={{ gap: "16px" }}
+        htmlFor={this.props.movie.id}
+      >
+        <input
+          className="form-control"
+          type="text"
+          value={this.props.movie.name}
+          id={this.props.movie.id}
+          onChange={(e) => {
+            this.props.onInputChange(this.props.movie.id, e.target.value);
+          }}
+        />
+        <button className="btn btn-primary" onClick={this.props.onButtonClick}>
+          X
+        </button>
+      </label>
+    );
+  }
+}
+
+export default MovieItem;

db457d75e85b0e049925fb071ee5c05a8dc04a23
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-18 16:49:52 +0600

    Added onFormChange/onFormSubmit funcs to MovieForm

--- src/components/MovieForm/MovieForm.tsx
+++ src/components/MovieForm/MovieForm.tsx
@@ -1,17 +1,48 @@
 import React from "react";
+import { Movie, MovieMutation } from "../../types";
 
-class MovieForm extends React.Component {
+interface State {
+  movie: MovieMutation;
+}
+
+interface Props {
+  onSubmit: (movie: Movie) => void;
+}
+class MovieForm extends React.Component<Props, State> {
+  state = {
+    movie: { name: ""},
+  };
+
+  onFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target;
+    this.setState(prev => {
+      return {
+        ...prev,
+        movie: {...prev.movie, [name]: value}
+      }
+    });
+  }
+
+  onFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    this.props.onSubmit({
+      id: (Math.random() + Math.random()).toString(),
+      ...this.state.movie
+    })
+    this.setState(prev => ({...prev, movie: {name: ''}}))
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.onFormSubmit}>
         <div className="mb-3">
           <label htmlFor="exampleInputEmail1" className="form-label">
             Movie name
           </label>
-          <input type="text" className="form-control" id="name" />
+          <input type="text" className="form-control" id="name" name="name" value={this.state.movie.name} onChange={this.onFormChange} />
         </div>
         <button type="submit" className="btn btn-primary">
-          Submit
+          Add
         </button>
       </form>
     );
